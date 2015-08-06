4800ef397e6ae1ec2fd065ba56193926031f04e9
Kyle Murphy <kmurph73@gmail.com>
2015-08-05 22:37:35 -0700

    about links go to safari

--- thisbeit/AboutViewController.swift
+++ thisbeit/AboutViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AboutViewController: UIViewController {
+class AboutViewController: UIViewController, UIWebViewDelegate {
   @IBOutlet weak var webV: UIWebView!
   @IBAction func tapClose(sender: AnyObject) {
     self.dismissViewControllerAnimated(true, completion: nil)
@@ -16,12 +16,22 @@ class AboutViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    webV.delegate = self
     // Load the BullsEye.html file into the web view.
     if let htmlFile = NSBundle.mainBundle().pathForResource("about", ofType: "html") {
       let htmlData = NSData(contentsOfFile: htmlFile)
       let baseURL = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath)
       webV.loadData(htmlData, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
     }
+  }
+  
+  func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+    println("request: \(request)")
+    if navigationType == UIWebViewNavigationType.LinkClicked {
+      UIApplication.sharedApplication().openURL(request.URL!)
+      return false;
+    }
+    return true;
   }
   
 }
